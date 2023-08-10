3bc27de7e864a2a51dfadd829d65fe9693f36122
Mathias Wakgari <mathiaswakgari12@gmail.com>
2023-08-10 12:04:56 +0300

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { Box, Grid, GridItem, HStack, Show } from "@chakra-ui/react";
+function App() {
+  return (
+    <Grid
+      templateAreas={{
+        base: `"navbar"
+              "main"`,
+        lg: `"navbar navbar"
+            "sidebar main"`,
+      }}
+      templateColumns={{
+        base: "1fr",
+        lg: "300px 1fr",
+      }}
+    >
+      <GridItem area={"navbar"} bgColor={"red"}>
+        NavBar
+      </GridItem>
+      <GridItem area={"main"} bgColor={"black"}>
+        Main
+      </GridItem>
+      <Show above="lg">
+        <GridItem area={"sidebar"} bgColor={"blue"}>
+          SideBar
+        </GridItem>
+      </Show>
+    </Grid>
+  );
+}
+export default App;
