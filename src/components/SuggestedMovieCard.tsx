68b46d348f8406a718d8de5f41f85c92c05eb02a
Mathias Wakgari <mathiaswakgari12@gmail.com>
2023-08-13 15:11:19 +0300

    Added hover color to Suggested Movies.

--- src/components/SuggestedMovieCard.tsx
+++ src/components/SuggestedMovieCard.tsx
@@ -7,7 +7,14 @@ interface Props {
 
 const SuggestedMovieCard = ({ movie }: Props) => {
   return (
-    <Card maxW={"xs"} border={"4px solid white"}>
+    <Card
+      maxW={"xs"}
+      border={"4px solid white"}
+      _hover={{
+        borderColor: "green",
+        transitionDuration: "550ms",
+      }}
+    >
       <Image width={"125px"} height={"187px"} src={movie.medium_cover_image} />
     </Card>
   );
