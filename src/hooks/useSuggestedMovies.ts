12ab00c2f356669982766a13df790331d71b6126
Mathias Wakgari <mathiaswakgari12@gmail.com>
2023-08-16 09:39:14 +0300

    Handled error in suggestedMovies hook.

--- src/hooks/useSuggestedMovies.ts
+++ src/hooks/useSuggestedMovies.ts
@@ -5,16 +5,19 @@ import apiClinet from "../services/api-clinet";
 
 const useSuggestedMovies = (id: string) => {
   const [suggestedMovies, setSuggestedMovies] = useState<Array<Movie>>([]);
+  const [error, setError] = useState("");
   useEffect(() => {
     const controller = new AbortController();
     apiClinet
       .get<FetchMovies>(`movie_suggestions.json?movie_id=${id}`)
-      .then((res) => setSuggestedMovies(res.data.data.movies));
+      .then((res) => setSuggestedMovies(res.data.data.movies))
+      .catch((e) => setError(e.message));
 
     return () => controller.abort();
   }, []);
   return {
     suggestedMovies,
+    error,
   };
 };
 
